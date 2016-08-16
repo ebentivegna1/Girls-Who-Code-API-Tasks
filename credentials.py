7628c9e9f68e08ebaeb44be3d54879c8b6cc80d2
Elaine Bentivegna <ebentivegna@gmail.com>
2016-08-16 13:24:32 -0400

    Create credentials.py

---
+++ credentials.py
@@ -0,0 +1,8 @@
+#Lists credentials needed for various API projects
+
+TUMBLR_CLIENT = pytumblr.TumblrRestClient(
+  'FozOJDiIYSYsK23oyg1DNjkM7BgfA3MFehb9uOzv9wfcyFzE93',
+  'vyBqvg2hVWC2gCjPO59kSxikh8c2Iq8BHSeLL55lUguNvTJ11o',
+  'UILvKnralDxvXckwC1WH2H8YXB6CK0kqtcGl8weoEsVjkSTe6i',
+  'DKLkma3Jh4fnHuNy8X9RC4z32cMn19mWjvknyPVuUbixdoTWBu'
+)
