540af8d37beaffd5fa3718e4f0f80681e9c0f5a7
Elaine Bentivegna <ebentivegna@gmail.com>
2016-08-16 14:16:44 -0400

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -4,15 +4,15 @@
 Step 1: Register and Application at https://www.tumblr.com/oauth/apps
 This will give you your client key and client secret
 
-Step 2: Use that info to get oauth token and key at
+Step 2: Use that info to get oauth token and secret at
 https://api.tumblr.com/console/calls/user/info
 
 Step 3: Fill in the relevant information below
 """
 import pytumblr
 
-consumer_key=''
-consumer_secret=''
+client_key=''
+client_secret=''
 oauth_token=''
 oauth_secret=''
 
