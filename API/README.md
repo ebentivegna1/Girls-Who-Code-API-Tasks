20c92bb0ddf41a6386da6d99e1852556f1f545c8
ELAINE BENTIVEGNA <ebentivegna@gmail.com>
2016-10-04 12:23:30 -0400

    moved into subfolder

---
+++ API/README.md
@@ -0,0 +1,8 @@
+# Girls-Who-Code-API-Tasks
+Simple API Projects for Girls Who Code
+
+These are simple use cases of various APIs to use in Girls Who Code Classes.
+
+The use cases focus on learning loops and functions within the context of using APIs.
+
+For tasks that require authentication, find directions and setup in `credentials.py`. Edit `credentials.py` with the proper credentials.
---
+++ API/spotify_top10_tracks.py
@@ -0,0 +1,16 @@
+#Simple use case of Spotify API
+#If you don't have spotipy installed, pip install spotipy before using this script
+#Go to Spotify, look up an artist and click on the three dots button next to the following button for that artist
+#Once you click on that button grab the "Spotify URI" for that artist
+#THe Spotify URI will be what you input as the artist_key in the function
+
+import spotipy
+
+spotify = spotipy.Spotify()
+
+def top_ten_tracks(artist_name, artist_key):
+	results = spotify.artist_top_tracks(artist_key)
+
+	print "Top 10 Tracks For:  " + artist_name
+	for track in results['tracks'][:10]:
+		print 'track  : ' + track['name']
